3ca7979419cfe555899183f211b3dce1c3a2cf21
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2024-10-26 16:08:32 +0530

    created layout

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from './tools/components/footer/footer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo';
