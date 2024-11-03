85a2fa5c4a6867dbeae3f3c5895e2c212c7bc822
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2024-11-03 19:26:49 +0530

    dev

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -3,6 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
     path: 'profile',
     loadComponent: () =>
       import('./components/profile/profile.component').then(
