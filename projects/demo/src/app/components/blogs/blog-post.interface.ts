560ee3a41cc096a3ed04adc020257dd8481d17e1
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2024-12-19 23:14:55 +0530

    blog updated

---
+++ projects/demo/src/app/components/blogs/blog-post.interface.ts
@@ -0,0 +1,6 @@
+export interface IBlogPost {
+  name: string;
+  description: string;
+  url: string;
+  date: string;
+}
--- projects/demo/src/app/components/blogs/blogs.component.ts
+++ projects/demo/src/app/components/blogs/blogs.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { IBlogPost } from './blog-post.interface';
 @Component({
   selector: 'app-blogs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blogs.component.html',
-  styleUrl: './blogs.component.scss'
+  styleUrl: './blogs.component.scss',
 })
 export class BlogsComponent {
-
+  blogPosts: IBlogPost[] = [
+    {
+      name: 'Angular Modularity Guide',
+      description:
+        'How to differentiate different type of angular modules and its Architecture guide ...',
+      url: 'https://medium.com/@ram.dev/angular-modularity-guide-7cae69c69a5d',
+      date: 'Jul 3, 2023',
+    },
+    {
+      name: 'Closure in JavaScript',
+      description:
+        'A closure is a combination of a function bundled together (enclosed) with references to its surrounding state ...',
+      url: 'https://yashjawade88.medium.com/closure-in-javascript-yjs-3dc1c3452ef9',
+      date: 'Jan 17, 2021',
+    },
+    {
+      name: 'Optimizing Data Flow in Angular Forms',
+      description:
+        'A streamlined approach for reducing code redundancy in large Angular forms and optimizing data flow patterns...',
+      url: 'https://medium.com/@ram.dev/optimising-data-flow-and-reducing-code-redundancy-in-large-angular-form-streamlined-approach-1cf0e701bc48',
+      date: 'Aug 15, 2023',
+    },
+  ];
 }
